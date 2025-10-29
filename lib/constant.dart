95254d8c275b51d8fd7a057744a3ffb7a0fe4c32
mostafa <moa10266@gmail.com>
2025-10-29 19:54:44 +0300

    init hive

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 const kprimrycolor = Color(0xFF5FE9D8);
+const kNotesBox = " Notes_Box";
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/constant.dart';
 import 'package:flutter_application_1/views/notes_view.dart';
+import 'package:hive_flutter/adapters.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox(kNotesBox);
+
   runApp(const MyApp());
 }
 
@@ -12,10 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-         fontFamily: "Poppins",
-         ),
+      theme: ThemeData(brightness: Brightness.dark, fontFamily: "Poppins"),
       home: const NotesView(),
     );
   }
